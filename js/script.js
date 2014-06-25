2cc5268955485d6d14a8676734b665798e2e2dc8
aaraneda <aaraneda@gmail.com>
2014-06-25 13:08:40 -0400

    ajuste JS

--- js/script.js
+++ js/script.js
@@ -32,13 +32,11 @@ $(document).ready(function(){
 		i = 0;
 		x = 0;
 		currentSlide 	   = $.deck('getSlide', to);
-		//console.log(currentSlide.attr("id"))
 		countElement = $(".fade", currentSlide).length;
-		//console.log(currentSlide.attr("id") + " " + countElement);
 		countElementBullet = $(".bullet", currentSlide).length;
 		countElementScale  = $(".scale", currentSlide).length;
 
-		if ($(".fade", currentSlide).css("display") == 'block') return false; 
+		//if ($(".fade", currentSlide).css("display") == 'block') return false; 
 		animateHead();
 			
 	}).bind('deck.init', function() {
@@ -93,7 +91,6 @@ $(document).ready(function(){
 	}
 
 	function animateScale(){
-		//$(".bullet", currentSlide).eq(i).animate({width:22, height:22}, 500);
 		if ($(".scale").length  <= 0 ) return false;
 
 		$(".scale", currentSlide).eq(x).delay(900).show( "scale", {percent: 100, direction: 'horizontal' }, 600 );
@@ -112,7 +109,7 @@ $(document).ready(function(){
 
 	var sw = 80;
 	function animateHeart(){
-		if (!$(".latido", currentSlide).length ) return false;
+		/*if (!$(".latido", currentSlide).length ) return false;
 		sw = sw == 80 ? 100 : 80;
 		
 		//$(".latido", currentSlide).eq(x).delay(900).animate( "scale", {percent: sw, direction: 'horizontal' }, 600, function(){ animateHeart(sw); } );
@@ -121,7 +118,7 @@ $(document).ready(function(){
           { to: {width: 84,height: 59} }, 1000, function(){ animateHeart(); } );
 		
 		 //$(".latido", currentSlide).effect( "scale", { percent: sw }, function(){ animateHeart(sw)} );						  
-		
+		*/
 	}	
 
 	function animateProgressBar(){
